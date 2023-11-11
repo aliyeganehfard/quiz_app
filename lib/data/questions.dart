1f4d541dd8585325b407da6431eb68fa5957d6a7
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-11-11 20:22:58 +0330

    add question and second page

---
+++ lib/data/questions.dart
@@ -0,0 +1,24 @@
+import 'package:quiz_app/model/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building block of Flutter UIs?',
+    ['Widget', 'Components', 'Blocks', 'Functions'],
+  ),
+  QuizQuestion(
+      'Second question',
+    ['one', 'two', 'three', 'four'],
+  ),
+  QuizQuestion(
+    'Third question?',
+    ['1', '2', '3', '4'],
+  ),
+  QuizQuestion(
+    'Fourth question?',
+    ['first', 'second', 'third', 'fourth'],
+  ),
+  QuizQuestion(
+    'Final question?',
+    ['java', 'c#', 'dart', 'python'],
+  ),
+];
---
+++ lib/quiz.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz_screen.dart';
+
+import 'start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  // Widget? activeScreen;
+
+  var activeScreen = 'start-screen';
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  void switchScreen() {
+    setState(() {
+      // activeScreen = const QuestionsScreen();
+      activeScreen = 'question-screen';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidget = activeScreen == 'start-screen'
+        ? StartScreen(switchScreen)
+        : const QuestionsScreen();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 73, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          // child: activeScreen,
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
